b516267b08b1af2089e23e734b03859451a26eb4
menuchabern <menucha9021@gmail.com>
2025-05-26 21:07:16 -0400

    initial commit

---
+++ WindowsFormsUtility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPUWindowsFormsFramework
+{
+    public class WindowsFormsUtility
+    {
+
+        public static void SetListBinding(ComboBox lst, DataTable sourcedt, DataTable targetdt, string tablename )
+        {
+            lst.DataSource = sourcedt;
+            lst.ValueMember = tablename + "ID";
+            lst.DisplayMember = lst.Name.Substring(3);
+            lst.DataBindings.Add("SelectedValue", targetdt, lst.ValueMember, false, DataSourceUpdateMode.OnPropertyChanged);
+        }
+
+        public static void SetControlBinding(Control ctrl, DataTable dt)
+        {
+            string propertyname = "";
+            string controlname = ctrl.Name.ToLower();
+            string controltype = controlname.Substring(0, 3);
+            string columnname = controlname.Substring(3);
+
+            switch (controltype)
+            {
+                case "txt":
+                case "lbl":
+                    propertyname = "text";
+                    break;
+                case "dtp":
+                    propertyname = "Value";
+                    break;
+            }
+            if (propertyname != "" && columnname != "")
+            {
+                ctrl.DataBindings.Add(propertyname, dt, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
+            }
+        }
+    }
+}
