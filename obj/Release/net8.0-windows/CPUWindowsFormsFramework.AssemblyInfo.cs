e6846a787c338cbb29de2402e72b5f398ef5f55b
menuchabern <menucha9021@gmail.com>
2025-11-05 16:40:37 -0500

    in middle of session 2.32 hw

--- obj/Release/net8.0-windows/CPUWindowsFormsFramework.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/CPUWindowsFormsFramework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPUWindowsFormsFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62d64450f190e568383c00c015dc8286aad38aca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fffa806b7151786af23e213ed85cf76642539f9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPUWindowsFormsFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPUWindowsFormsFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
